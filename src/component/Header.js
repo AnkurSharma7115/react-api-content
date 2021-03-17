9816938b403e0dee5722f0cdd1becc47b907b9e4
Ankur Sharma <sharma18ankur@gmail.com>
2021-03-17 14:30:06 +0530

    Nav Item and Styles

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './Header.css'
 function Header() {
     return (
-        <div className="App-header">
-            It is you Header for Navigation
+        <div className="App-header">            
+                <ul className="navbar-link">
+                    <li className="navbar-link-item">Home</li>
+                    <li className="navbar-link-item">About</li>
+                    <li className="navbar-link-item">Blog</li>
+                </ul>
         </div>
-    )
+    );
 }
 
 export default Header
