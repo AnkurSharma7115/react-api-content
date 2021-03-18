c1f41b5eff80bc1500f898bd5649a55fdd1b9390
Ankur Sharma <sharma18ankur@gmail.com>
2021-03-18 18:34:32 +0530

    Added Side-Nav, Routes and update few Styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
-import Header from './component/Header';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-import Home from './component/Home';
+import Header from './component/Header';
+import Home from './component/SideNav/Home';
 import About from './component/About';
 import Contact from './component/Contact';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       images : []
+    }
+  }
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+    .then(response => response.json())
+    .then(data => {
+      this.setState(prevState =>({
+        images : prevState.images=data
+      }))
+    })
+
+  }
     render() {
         return (
             <div className="App">
@@ -15,7 +32,7 @@ class App extends Component {
                     <Switch>
                         <Route path="/contact" component={Contact}></Route>
                         <Route path="/about" component={About}></Route>
-                        <Route path="/" component={Home}></Route>
+                        <Route  path="/" render={() => <Home images={this.state.images}/>}></Route>
                     </Switch>
                 </Router>
             </div>
---
+++ src/component/SideNav/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Switch, Route } from "react-router-dom";
+import './Nav.css'
+import SideBar from './SideBar'
+function Home(props) {
+      
+    const items = () => {
+        return (
+            <div className="home">
+                {props.images.map((data) => (
+                    <div key={data.id} style={{ margin: "30px" }}>
+                        <div>{`Album-ID: ${data.albumId}`}</div>
+                        <div>{`Title: ${data.title}`}</div>
+                        <div>{`URL: ${data.url}`}</div>
+                        <img src={data.url} alt={data.titile}/>
+                    </div>
+                ))}
+            </div>
+        );
+    }; 
+     return (
+         <div>
+             <SideBar />
+             <Switch>
+                 <Route path="/category1" component={items} />
+             </Switch>
+         </div>
+     );
+}
+
+export default Home
