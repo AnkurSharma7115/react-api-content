61917ed5357163b7a6f10de67b78e84ab09edc28
Ankur Sharma <sharma18ankur@gmail.com>
2021-03-26 19:09:18 +0530

    Using Axios and Styled Component

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,17 +1,55 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import './Header.css'
+import styled from 'styled-components'
+// import './Header.css'
+const AppHeader = styled.div
+`background: rgba(112, 128, 144,1);
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 5px 10px;
+z-index: 1;
+width:100%;
+min-height: 50px;
+position: fixed;
+top: 0;
+left: 0;
+`;
+const NavLinks =styled.ul
+` display: flex;
+list-style: none;
+`;
+
+const NavLinkItem = styled(Link)
+`text-decoration: none;
+margin: 5px 20px 5px 0;
+padding: 5px 30px;
+color:black;
+font-size: 18px;
+ cursor: pointer;
+ border: 1px solid black;
+ border-radius: 4px;
+ &:hover{
+    border-radius: 4px;
+      background-color: #222;
+      color: aliceblue;  
+      transition: 0.2s all; 
+ }
+ &:active{
+    transform: scale(0.96); 
+ }
+`;
 function Header() {
     return (
-        <div className="App-header">            
-                <ul className="navbar-link">
-                    <Link to='/home' className="navbar-link-item">Home</Link>
-                    <Link to='/about' className="navbar-link-item">About</Link>
-                    <Link to='/contact' className="navbar-link-item">Contact</Link>
-
-                </ul>
-        </div>
-    );
+             <AppHeader>      
+               <NavLinks>
+               <NavLinkItem to='/home' className="navbar-link-item">Home</NavLinkItem>
+                    <NavLinkItem to='/about' className="navbar-link-item">About</NavLinkItem>
+                    <NavLinkItem to='/contact' className="navbar-link-item">Contact</NavLinkItem>
+               </NavLinks>              
+            </AppHeader>
+       
+    )
 }
-
+   
 export default Header
--- src/component/SideNav/Home.js
+++ src/component/SideNav/Home.js
@@ -3,6 +3,7 @@ import { Switch, Route, useRouteMatch } from "react-router-dom";
 import "./Nav.css";
 import SideBar from "./SideBar";
 import Category from "./Category";
+import axios from "axios";
 function Home(props) {
     let match = useRouteMatch();
     
@@ -10,31 +11,46 @@ function Home(props) {
     const [category, setCategory] = useState([]);
 
     useEffect(() => {
-        Promise.all([fetch(" https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=263a26ec36bcbc6698026735c6c21aff&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"),
-                      fetch("https://picsum.photos/v2/list") ])
+        const urlImage = "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=263a26ec36bcbc6698026735c6c21aff&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
+        const urlCategory = "https://picsum.photos/v2/list"
         
-        .then(([response1, response2]) => {
-            return Promise.all([response1.json(), response2.json()])
-        })
-        .then(([data1, data2]) => {
-                    data1.photos.photo.map((item) => {
-                       return item.urlw ="https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg"
-                       
-                    })
-                    setImage(data1.photos.photo)
-                    setCategory(data2)
+        Promise.all([axios.get(urlImage), axios.get(urlCategory)])
+            .then(([response1, response2]) => {
+               
+                response1.data.photos.photo.map((item) => {
+                    return (item.urlWWW ="https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id +"_" +item.secret +".jpg");
+                });
+
+                setImage(response1.data.photos.photo);
+                setCategory(response2.data);
+            })
+            .catch((errors) => {
+                console.error(errors);
             });
+        // Promise.all([fetch(" https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=263a26ec36bcbc6698026735c6c21aff&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"),
+        //               fetch("https://picsum.photos/v2/list") ])
+        
+        // .then(([response1, response2]) => {
+        //     return Promise.all([response1.json(), response2.json()])
+        // })
+        // .then(([data1, data2]) => {
+        //             data1.photos.photo.map((item) => {
+        //                return item.urlw ="https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg"
+                       
+        //             })
+        //             setImage(data1.photos.photo)
+        //             setCategory(data2)
     }, []);
     
     const items = () => {
         return (
-            <div className="home">{console.log(image)}
+            <div className="home">
                 {image.map((data) => (
                     <div key={data.id} className="responsive">
                         <div className="gallery">
-                            <a href={data.urlw} target="blank">
+                            <a href={data.urlWWW} target="blank">
                                 <img
-                                    src={data.urlw}
+                                    src={data.urlWWW}
                                     alt={data.title}
                                 />
                             </a>
